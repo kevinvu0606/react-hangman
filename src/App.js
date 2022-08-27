2698ad802c247479dd286de5329e7e061a26d057
Kevin <kevin.vu06@gmail.com>
2022-08-27 22:44:15 +1000

    feat: added es lint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import Header from './components/Header'
-import Figure from './components/Figure'
-import WrongLetters from './components/WrongLetters'
-import Word from './components/Word'
-import Notification from './components/Notification'
-import PopUp from './components/PopUp'
+import React, {useState} from 'react';
+import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+import Notification from './components/Notification';
+import PopUp from './components/PopUp';
 import './App.css';
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+const selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const playable = true;
+
+const correctLetters = [];
+const wrongLetters = [];
+
+
 function App() {
+  const [playable, setPlayable] = useState(true);
+
   return (
-  <>
-    <Header />
-    <div className= "game container">
-      <Figure />
-      <WrongLetters />
-      <Word />
-    </div>
-  </>
+    <>
+      <Header />
+      <div className= "game container">
+        <Figure />
+        <WrongLetters />
+        <Word />
+      </div>
+    </>
   );
 }
- 
-export default App; 
+
+export default App;
